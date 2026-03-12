fead4ac682f3a4feedd646509e9a288bf7333c5c
Enrique Corral <enriquecorral97@gmail.com>
2026-03-12 00:33:41 -0600

    Convertir a async function

--- src/peliculasData.js
+++ src/peliculasData.js
@@ -1,27 +1,25 @@
 const API_KEY = import.meta.env.VITE_API_KEY;
 
-export function obtenerPelicula() {
-
+export async function obtenerPelicula() {
   let dataResponse = [];
   let promises = [];
 
   for (let i = 1; i < 4; i++) {
-
     const URL = `https://www.omdbapi.com/?apikey=${API_KEY}&s=movie&page=${i}`;
 
     const request = fetch(URL)
-      .then(response => {
+      .then((response) => {
         if (!response.ok) {
           throw new Error("Error en la petición");
         }
         return response.json();
       })
-      .then(data => {
+      .then((data) => {
         if (data.Search) {
           dataResponse.push(...data.Search);
         }
       })
-      .catch(error => {
+      .catch((error) => {
         console.error("Error:", error);
       });
 
